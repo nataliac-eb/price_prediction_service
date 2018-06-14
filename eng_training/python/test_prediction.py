508d833b38b85294159c77ff062eda72b9dfba33
Natalia C <nataliac@eventbrite.com>
2018-06-14 10:28:48 -0300

    dunders!

--- eng_training/python/test_prediction.py
+++ eng_training/python/test_prediction.py
@@ -10,10 +10,12 @@ class test_prediction():
         self.category = "101"
 
     def statistics_calc(self, data):
+        # es una buena idea calcularlo a mano
         return {"mean":statistics.mean(data), 'standard_deviation':statistics.stdev(data)}
 
     def test_result_by_country(self):
         precios = price(self.country, self.category)
         data = price.load_data()
         calc = self.statistics_calc(data)
+        self.assertEqual(precios, calc)
 
